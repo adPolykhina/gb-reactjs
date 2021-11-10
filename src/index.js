b911c3ef07f1c6b4bac6a23256a04c46d6d054e3
adPolykhina <adpolykhina@gmail.com>
2021-11-10 23:03:56 +0300

    New message component

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Components/App/app';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
