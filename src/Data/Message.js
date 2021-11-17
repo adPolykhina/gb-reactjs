83d977bba64850d4afd10d3b4d9555ae91eeef2c
adPolykhina <adpolykhina@gmail.com>
2021-11-17 20:17:08 +0300

    Some fixes

--- src/Data/Message.js
+++
@@ -1,6 +0,0 @@
-export class Message {
-    constructor(text, author) {
-        this.author = author;
-        this.text = text;
-    }
-}
--- src/View/App/appView.js
+++ src/View/App/appView.js
@@ -3,7 +3,6 @@ import List from '@mui/material/List';
 import { Grid, TextField, FormGroup, Divider } from '@mui/material';
 import Button from '@mui/material/Button';
 
-import { Message } from '../../Data/Message';
 import MessageView from '../Message/messageView';
 
 import "./list.css";
@@ -14,15 +13,14 @@ function App() {
     // App states
     const [ messages, setMessages ] = useState([]);
     const [newMessage, setNewMessage ] = useState("");
-    const [ author, setAuthor ] = useState("");
 
     // effect for recieving a message from robot
     useEffect(
         () => {
             if (messages.length){
-                const lastAutor = messages[messages.length - 1].author;
+                const lastAutor = messages[messages.length - 1]['author'];
                 if (lastAutor !== "robot")
-                    setTimeout(() => setMessages([...messages, new Message("Hello " + lastAutor, "robot")]), 1500);
+                    setTimeout(() => setMessages([...messages, {'author': 'robot', 'message': 'Hello' + lastAutor}]), 1500);
             }
         },
      [messages]
@@ -30,14 +28,13 @@ function App() {
 
     // functions for change states 
     const changeMessage = (event) => { setNewMessage(event.target.value) };
-    const changeAuthor = (event) => { setAuthor(event.target.value) };
     const sendNewMessage = () => { 
-        if (newMessage.trim() && author.trim()){
-            setMessages([...messages, new Message(newMessage, author)]);
+        if (newMessage.trim()){
+            setMessages([...messages, {'author': 'Anastasia', 'message': newMessage}]);
             setNewMessage("");
         }
         else 
-            alert("Message or author cannot be empty!");
+            alert("Message cannot be empty!");
     };
 
     return(
@@ -48,23 +45,15 @@ function App() {
             <Grid item xs={12} justifyContent="center" display="flex">
                 {messages.length ?
                 <List className="List">
-                    {messages.map((message, index) => {
-                        if (index !== messages.length - 1) {
-                            return (<><MessageView textMessage={message.text} author={message.author}/>
-                                <Divider variant="middle" component="li" /></>)
-                        }
-                        else {
-                            return <MessageView textMessage={message.text} author={message.author}/>
-                        }
-                
-                    }
+                    {messages.map((message, index) => 
+                            <><MessageView textMessage={message['message']} author={message['author']}/>
+                            {index !== messages.length - 1 && <Divider variant="middle" component="li" />}</>
                     )}
                 </List> : undefined
                 }
             </Grid>
             <Grid item xs={12} md={8} justifyContent="center" display="flex">
                 <FormGroup row={true}>
-                    <TextField id="nameFiled" value={author} onChange={changeAuthor} placeholder="Your name"></TextField>
                     <TextField id="tmessageField" value={newMessage} onChange={changeMessage} placeholder="Your message"></TextField>
                     <Button variant="contained" onClick={sendNewMessage} className="send-button">Send</Button>
                 </FormGroup>
